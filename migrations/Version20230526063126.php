9556c0e8b5cbbe267c6b0c417ea929062e52875e
ANIMASHAUN Michael <michaeloncode@gmail.com>
2023-05-27 23:06:16 +0100

    Default Site Options Added on First Migration

--- migrations/Version20230526063126.php
+++ migrations/Version20230526063126.php
@@ -23,6 +23,11 @@ final class Version20230526063126 extends AbstractMigration
         $this->addSql('ALTER TABLE user ADD photo VARCHAR(255) DEFAULT NULL');
         $this->addSql("INSERT INTO `user` (`id`, `username`, `roles`, `password`, `email`, `photo`) VALUES
         (default, 'administrator', '[\"ROLE_ADMIN\"]', '$2y$13\$FxWcLO4NOopenCeuLVuFbu4kH01A49a/Fg8ajvCDOIin21a8otVuK', 'michaeloncode@gmail.com', '');");
+        $this->addSql("INSERT INTO `parameters` (`id`, `label`, `value`) VALUES
+        (45, 'siteName', 'Transfer Manager'),
+        (46, 'siteDescription', 'Imperdiet debitis in! Porttitor, repellendus harum pede! Eu amet aenean culpa, molestias. Quisquam doloremque optio varius. Malesuada. Quod, expedita animi.'),
+        (47, 'siteCurrency', 'USD'),
+        (48, 'siteLogo', 'football-6470f037b0a35.png');");
     }
 
     public function down(Schema $schema): void
